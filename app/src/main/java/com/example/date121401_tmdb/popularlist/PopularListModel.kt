d52cb7a3b2029c3a47376fd383db72b3b13831d6
mygit <myemail@mail.com>
2020-12-14 17:13:00 +0800

    temp save, list item xml.

--- app/src/main/java/com/example/date121401_tmdb/popularlist/PopularListModel.kt
+++ app/src/main/java/com/example/date121401_tmdb/popularlist/PopularListModel.kt
@@ -4,3 +4,12 @@ data class PopularListModel(
     val id: Int,
     val title: String,
 )
+
+
+val puplarListDataSource1: List<PopularListModel> = listOf(
+    PopularListModel(1, "Movie 1"),
+    PopularListModel(2, "Movie 2"),
+    PopularListModel(3, "Movie 3"),
+    PopularListModel(4, "Movie 4"),
+    PopularListModel(5, "Movie 5"),
+)
