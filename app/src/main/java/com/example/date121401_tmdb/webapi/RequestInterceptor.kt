5466fb662a30a74a768bb6d6cd25e57079e6e80e
mygit <myemail@mail.com>
2020-12-15 18:19:57 +0800

    ok, BuildConfig.API_KEY.

--- app/src/main/java/com/example/date121401_tmdb/webapi/RequestInterceptor.kt
+++ app/src/main/java/com/example/date121401_tmdb/webapi/RequestInterceptor.kt
@@ -2,6 +2,7 @@ package com.example.date121401_tmdb.webapi
 
 import android.os.Parcel
 import android.os.Parcelable
+import com.example.date121401_tmdb.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -11,7 +12,7 @@ class RequestInterceptor() : Interceptor {
 
         // attach query string with apk_key.
         val url = request.url.newBuilder()
-            .addQueryParameter("api_key", "apikey")
+            .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
             .build()
 
         return chain.proceed(request)
