6ff0f8ae4a0a0bb5a738be948740241039143a58
mygit <myemail@mail.com>
2020-12-15 11:45:10 +0800

    temp save, end MovieDetailResponse.kt.

---
+++ app/src/main/java/com/example/date121401_tmdb/webapi/model/Genre.kt
@@ -0,0 +1,6 @@
+package com.example.date121401_tmdb.webapi.model
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/example/date121401_tmdb/webapi/model/ProductionCountry.kt
@@ -0,0 +1,6 @@
+package com.example.date121401_tmdb.webapi.model
+
+data class ProductionCountry(
+    val iso_3166_1: String,
+    val name: String
+)
