6471bca23d4c6fce26f2c991e3133b437c9535e7
mygit <myemail@mail.com>
2020-12-15 14:53:06 +0800

    ok, item click handler.\n prepare request.

--- app/src/main/java/com/example/date121401_tmdb/popularlist/PopularListAdapter.kt
+++ app/src/main/java/com/example/date121401_tmdb/popularlist/PopularListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.date121401_tmdb.popularlist
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,7 +21,7 @@ class PopularListAdapter(private val dataList: List<PopularListItemModel>) : Rec
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val h = holder as ItemViewHolder
         val data = dataList[position]
-        h.onBind(data)
+        h.onBind(data, position)
     }
 
     override fun getItemCount(): Int {
@@ -29,14 +30,24 @@ class PopularListAdapter(private val dataList: List<PopularListItemModel>) : Rec
 
     /////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////// ViewHolder.
-    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //
+        private var midxAdapter: Int = -1        // index of adapter position
         private var txvId: TextView = itemView.findViewById(R.id.txvId_PopularListItem)
         private var txvTitle: TextView = itemView.findViewById(R.id.txvTitle_PopularListItem)
 
-        fun onBind(data: PopularListItemModel) {
+
+        fun onBind(data: PopularListItemModel, position: Int) {
+            midxAdapter = position
             txvId.text = data.id.toString()
             txvTitle.text = data.title
         }
+
+        ////////// Click Event Handler
+        private var clickListener = View.OnClickListener{
+            Log.i("ItemViewHolder", "idx: $midxAdapter clicked.")
+        }.also { itemView.setOnClickListener(it) }
+        ////////// Click Event Handler end
 
     } // end class ItemViewHolder.
     ///////////////////////////////////////////////////////////////// ViewHolder end.
