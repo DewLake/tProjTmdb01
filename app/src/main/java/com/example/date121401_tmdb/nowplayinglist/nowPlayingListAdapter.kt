49a3e88dc9fc2eea201447b90213ec971b1b19ef
mygit <myemail@mail.com>
2020-12-22 12:45:13 +0800

    temp save, glide fluid.

--- app/src/main/java/com/example/date121401_tmdb/nowplayinglist/nowPlayingListAdapter.kt
+++ app/src/main/java/com/example/date121401_tmdb/nowplayinglist/nowPlayingListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.date121401_tmdb.nowplayinglist
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +8,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.date121401_tmdb.R
 import com.example.date121401_tmdb.webapi.TmdbRepository
 
@@ -49,11 +54,27 @@ class nowPlayingListAdapter(private var dataList: List<NowPlayingListItemModel>)
             // Image:
             // val uri = ContextCompat.getDrawable(itemView.context, R.drawable.ic_launcher_background)
             // Glide.with(itemView).load(uri).into(imvImage)
-            val uri = TmdbRepository.GetImageUrl(data.imgSrc)
-            Glide.with(itemView).load(uri).into(imvImage)
+            loadImage(data)
         } // end onBind().
 
+        private fun loadImage(data: NowPlayingListItemModel) {
+            val uri = TmdbRepository.GetImageUrl(data.imgSrc)
+            val requestListener = object : RequestListener<Drawable>{
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    TODO("Not yet implemented")
+                }
 
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    TODO("Not yet implemented")
+                }
+
+            }
+
+            Glide.with(itemView)
+                    .load(uri)
+                    .listener(requestListener)
+                    .into(imvImage)
+        } // end loadImage().
     } // end class ItemViewHolder.
     ///////////////////////////////////////////////////////////////// ViewHolder end.
 } // end class BannerAdapter.
